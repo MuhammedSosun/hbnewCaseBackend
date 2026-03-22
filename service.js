0bb6a0a0de20a4c8e056c8f99111d09de6a97d59
Muhammed Sosun <muhammedsosun06@gmail.com>
2026-03-22 15:29:20 +0300

    cors address değişti

--- service.js
+++ service.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://hepsiburadacase1.netlify.app"],
+    origin: ["http://localhost:5173", "https://hepsiburadacase.netlify.app/"],
   }),
 );
 app.use(express.json());
